82ec8ada6ba8a61ac4c59fed0fc472e3326e98e1
Slava Kisel <primazp@icloud.com>
2018-07-13 09:04:50 +0300

    Add deploy scripts

---
+++ deploy-local-staging.sh
@@ -0,0 +1,2 @@
+scp -r ./libs/* root@meeting-staging.mastermindbetter.com:/usr/share/jitsi-meet/libs
+scp ./css/all.css root@meeting-staging.mastermindbetter.com:/usr/share/jitsi-meet/css
---
+++ deploy-local.sh
@@ -0,0 +1,2 @@
+scp -r ./libs/* root@meeting.mastermindbetter.com:/usr/share/jitsi-meet/libs
+scp ./css/all.css root@meeting.mastermindbetter.com:/usr/share/jitsi-meet/css
